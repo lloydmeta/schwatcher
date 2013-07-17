51e019377db7955ea5d6a59cccfbdd88ca87b9a2
Lloyd <lloydmeta@gmail.com>
2013-07-17 11:08:28 +0900

    Add tests for recursive adding

--- src/test/scala/com/beachape/filemanagement/MonitorActorSpec.scala
+++ src/test/scala/com/beachape/filemanagement/MonitorActorSpec.scala
@@ -5,8 +5,9 @@ import org.scalatest.BeforeAndAfter
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
 import akka.testkit.{TestActorRef, TestKit}
-import java.nio.file.{WatchEvent, Path}
+import java.nio.file.{Files, WatchEvent, Path}
 import java.nio.file.StandardWatchEventKinds._
+import java.nio.file.Path
 
 class MonitorActorSpec extends TestKit(ActorSystem("testSystem"))
   with FunSpec
@@ -14,9 +15,16 @@ class MonitorActorSpec extends TestKit(ActorSystem("testSystem"))
   with BeforeAndAfter {
 
   val tempFile = java.io.File.createTempFile("fakeFile", ".log")
-  val monitorActorRef = TestActorRef(new MonitorActor)
-  val monitorActor = monitorActorRef.underlyingActor
   val dummyFunction: Path => Unit = { (path: Path) =>  val bleh = "lala"}
+
+  var monitorActorRef = TestActorRef(new MonitorActor)
+  var monitorActor = monitorActorRef.underlyingActor
+
+  after {
+    // Create a new actor for each test
+    monitorActorRef = TestActorRef(new MonitorActor)
+    monitorActor = monitorActorRef.underlyingActor
+  }
 
   describe("methods testing") {
 
@@ -30,6 +38,41 @@ class MonitorActorSpec extends TestKit(ActorSystem("testSystem"))
         monitorActor.addPathCallback(ENTRY_CREATE, tempFile.toPath, dummyFunction)
         monitorActor.callbacksForPath(ENTRY_CREATE, tempFile.toPath).map(callbacks =>
           callbacks should contain (dummyFunction))
+      }
+
+    }
+
+    describe("#recursivelyAddPathCallback") {
+
+      val tempDirPath = Files.createTempDirectory("root")
+      val tempDirLevel1Path = Files.createTempDirectory(tempDirPath, "level1")
+      val tempDirLevel2Path = Files.createTempDirectory(tempDirLevel1Path, "level2")
+      val tempFileInTempDir = Files.createTempFile(tempDirPath, "hello", ".there")
+
+      it("should return the path used for registration") {
+        monitorActor.recursivelyAddPathCallback(ENTRY_CREATE, tempDirPath, dummyFunction) should be(tempDirPath)
+      }
+
+      it("should add callbacks for all folders that exist under the path given") {
+        monitorActor.recursivelyAddPathCallback(ENTRY_CREATE, tempDirPath, dummyFunction)
+        monitorActor.callbacksForPath(ENTRY_CREATE, tempDirLevel1Path).map(callbacks =>
+          callbacks should contain (dummyFunction))
+        monitorActor.callbacksForPath(ENTRY_CREATE, tempDirLevel2Path).map(callbacks =>
+          callbacks should contain (dummyFunction))
+      }
+
+      it("should add callbacks for a file path") {
+        monitorActor.recursivelyAddPathCallback(ENTRY_CREATE, tempFileInTempDir, dummyFunction)
+        monitorActor.callbacksForPath(ENTRY_CREATE, tempFileInTempDir).map(callbacks =>
+          callbacks should contain (dummyFunction))
+      }
+
+      it("should not add callbacks recursively if given a file path") {
+        monitorActor.recursivelyAddPathCallback(ENTRY_CREATE, tempFileInTempDir, dummyFunction)
+        monitorActor.callbacksForPath(ENTRY_CREATE, tempDirLevel1Path).map(callbacks =>
+          callbacks should not contain (dummyFunction))
+        monitorActor.callbacksForPath(ENTRY_CREATE, tempDirLevel2Path).map(callbacks =>
+          callbacks should not contain (dummyFunction))
       }
 
     }
