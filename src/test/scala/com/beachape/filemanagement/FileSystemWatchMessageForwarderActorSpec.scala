221563d983d8e7fc0731b4598a3e94a3dcc11202
Lloyd <lloydmeta@gmail.com>
2016-12-01 23:17:00 +0900

    Switch to FunSpec and Eventually for clarity

--- src/test/scala/com/beachape/filemanagement/FileSystemWatchMessageForwarderActorSpec.scala
+++ src/test/scala/com/beachape/filemanagement/FileSystemWatchMessageForwarderActorSpec.scala
@@ -9,9 +9,8 @@ import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import com.beachape.filemanagement.Messages.EventAtPath
 import com.sun.nio.file.SensitivityWatchEventModifier
 import org.scalatest._
-
-import scala.concurrent.duration._
-import scala.language.postfixOps
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Seconds, Span}
 
 /**
   * Created by arunavs on 9/27/16.
@@ -21,10 +20,13 @@ import scala.language.postfixOps
 class FileSystemWatchMessageForwarderActorSpec
     extends TestKit(ActorSystem("FSWatchTest"))
     with ImplicitSender
-    with WordSpecLike
+    with FunSpecLike
     with Matchers
     with BeforeAndAfterAll
-    with BeforeAndAfterEachTestData {
+    with Eventually {
+
+  implicit override val patienceConfig =
+    PatienceConfig(timeout = scaled(Span(100, Seconds)), interval = scaled(Span(5, Millis)))
 
   private val locationDir           = "./src/test/permdata"
   private val locationDirFileHandle = new File(locationDir)
@@ -44,26 +46,37 @@ class FileSystemWatchMessageForwarderActorSpec
     TestKit.shutdownActorSystem(system)
   }
 
-  "An FileSystemWatchMessageForwardingActor" must {
-    "send a changed Dir Message of modified type file when an existing file is modified" in {
-      val existingFileName = "existingFile.txt"
-      val existingFile     = new File(locationDir + File.separator + existingFileName)
-      printTestToFile(existingFile)
-      within(100 seconds) {
-        val filePath = existingFile.getAbsoluteFile.toPath
-        expectMsg(EventAtPath(ENTRY_MODIFY, filePath))
-      }
+  it("should send a changed Dir Message of modified type file when an existing file is modified") {
+    val existingFileName = "existingFile.txt"
+    val existingFile     = new File(locationDir + File.separator + existingFileName)
+    printTestToFile(existingFile)
+    eventually {
+      val filePath = existingFile.getAbsoluteFile.toPath
+      expectMsg(EventAtPath(ENTRY_MODIFY, filePath))
     }
+  }
 
-    "send a changed Dir Message of created type file when a new file is created" in {
-      val newFileName = "newFile.txt"
-      val newFile     = new File(locationDir + File.separator + newFileName)
-      newFile.createNewFile()
-      newFile.deleteOnExit()
-      within(100 seconds) {
-        val filePath = newFile.getAbsoluteFile.toPath
-        expectMsg(EventAtPath(ENTRY_CREATE, filePath))
-      }
+  it("should send a changed Dir Message of created type file when a new file is created") {
+    val newFileName = "newFile.txt"
+    val newFile     = new File(locationDir + File.separator + newFileName)
+    newFile.createNewFile()
+    newFile.deleteOnExit()
+    eventually {
+      val filePath = newFile.getAbsoluteFile.toPath
+      expectMsg(EventAtPath(ENTRY_CREATE, filePath))
+    }
+  }
+
+  // TODO figure out why this fails. (Perhaps we need to isolate them in fixtures in order for expectMsg to work)
+  it("should send a changed Dir Message of created type file when a new file is deleted") {
+    val newFileName = "newFile2"
+    val newFile     = new File(locationDir + File.separator + newFileName)
+    newFile.createNewFile()
+    Thread.sleep(10000)
+    newFile.delete()
+    eventually {
+      val filePath = newFile.getAbsoluteFile.toPath
+      expectMsg(EventAtPath(ENTRY_DELETE, filePath))
     }
   }
 
